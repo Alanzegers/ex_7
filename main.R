d5354319537e29ed5e0672ab5079c03f005a1221
Alanzegers <alanzegers@gmail.com>
2016-01-13 10:01:14 +0100

    Update main.R
    added plot

--- main.R
+++ main.R
@@ -8,44 +8,6 @@ library(sp)
 library(rgdal)
 library(rgeos)
 library(raster)
-
-### Download and unzip files
-download.file(url = 'https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data/MODIS.zip', destfile = 'data/modis.zip', method = 'wget')
-unzip('data/modis.zip', overwrite = TRUE, exdir = 'data/')
-
-
-NDVI <- list.files(path='data/', pattern = glob2rx('*.grd'), full.names=TRUE)
-
-NDVIstack <- stack(NDVI)
-
-nlCity <- raster::getData('GADM',country='NLD', level=2)
-nlCity@data <- nlCity@data[!is.na(nlCity$NAME_2),] 
-Lvl2Contour <- spTransform(nlCity, CRS(proj4string(NDVIstack)))
-
-NDVIJan <- NDVIstack[[1]]
-NDVIAug <- NDVIstack[[8]]
-plot(NDVIAug)
-plot(Lvl2Contour, add=T)
-
-
-##
-NDVICrop <- crop(NDVIbrick, Level2Contour)
-plot(NDVICrop)
-NDVISub <- mask(NDVIbrick, Level2Contour)
-
-class(Level2Contour)
-plot(Level2Contour, add=T)
-########################################
-### Alan Zegers & Stijn Wijdeven
-### Teamname*
-### January 2016
-
-### Exercise 7 Greenest city of the Netherlands
-
-library(sp)
-library(rgdal)
-library(rgeos)
-library(raster)
 if(!require(leaflet)) {
 	install.packages("leaflet")
 }
@@ -56,35 +18,25 @@ unzip('data/modis.zip', overwrite = TRUE, exdir = 'data/')
 
 
 NDVI <- list.files(path='data/', pattern = glob2rx('*.grd'), full.names=TRUE)
-
 NDVIstack <- stack(NDVI)
 
-nlCity <- raster::getData('GADM',country='NLD', level=2, path= 'data/') 
-nlCity@data <- nlCity@data[!is.na(nlCity$NAME_2),] 
+nlCity <- raster::getData('GADM',country='NLD', level=2, path= 'data/')
+nlCity@data <- nlCity@data[!is.na(nlCity$NAME_2),]
 Lvl2Contour <- spTransform(nlCity, CRS(proj4string(NDVIstack)))
 
 NDVIMask <-mask(NDVIstack, Lvl2Contour)
-plot(NDVIMask)
 
 NDVIJan <- NDVIMask[[1]]
 NDVIAug <- NDVIMask[[8]]
 
-January <- NDVIMask$January
-August <- NDVIMask$August
-
-
-plot(NDVIAug)
-plot(Lvl2Contour, add=T)
-
-#Calculate average
+###Calculate average
 NDVIavg <- calc(NDVIMask, fun=mean)
 
-
-#Extract
+###Extract
 ExtractJan <- extract(NDVIJan, Lvl2Contour, df=TRUE, fun=mean)
 ExtractAug <- extract(NDVIAug, Lvl2Contour, df=TRUE, fun=mean)
 Extractavg <- extract(NDVIavg, Lvl2Contour, df=TRUE, fun=mean)
-
+plot(Extractavg)
 ##Find cities
 #january
 MaxJan <- subset(ExtractJan, ExtractJan$January == max(ExtractJan$January, na.rm = TRUE))
@@ -99,20 +51,14 @@ Maxavg <- subset(Extractavg, Extractavg$layer == max(Extractavg$layer, na.rm = T
 IDfind3 <- subset(Lvl2Contour, Lvl2Contour$ID_2 == Maxavg[,1])
 city_average <- IDfind3$NAME_2
 
-### print city name 
-paste("The greenest city in January is", city_january, 
-			", The greenest city in August is", city_august,  
+### print city name
+paste("The greenest city in January is" ,city_january,
+			", The greenest city in August is", city_august, 
 			", The greenest city overall is", city_average)
 ### Plot
-#test
-plot(Maxavg)
-leaflet()
-
-plotmap <- leaflet()
-plotmap <- addTiles(plotmap)
-plotmap <- addPolygons(plotmap,data = nlCity, opacity = 2, color = "Green", weight = 1, fillColor = "white") # To view the specified region  
-spplot(Lvl2Contour, zcol= Extractavg)
-plotmap
-spplot(NDVIJan, zcol = 'January', main = 'January NDVI')
-spplot(NDVIAug, zcol = 'August', main = 'August NDVI', add=T)
-spplot(Lvl2Contour, add=TRUE)
+plot(NDVIavg, main = paste("january greenest city:",city_january,
+													 ", August:", city_august, 
+													 ", overall:", city_average))
+plot(IDfind,  add=T)
+plot(IDfind2, add=T)
+plot(IDfind3, add=T)
